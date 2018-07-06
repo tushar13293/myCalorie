1cd22f6070142759e31599ead5d5b81235b98ad7
tushar <tushargarg495@gmail.com>
2018-07-06 21:16:02 +0200

    commit

---
+++ Final/MyCalorie/Assets/Scripts/BedroomMenuTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class BedroomMenuTrigger : MonoBehaviour {
+
+	Collider2D m_ObjectCollider;
+    public Image Popup;
+    public Text KitchenPopupText;
+    public Text GymPopupText;
+    public Text BedroomPopupText;
+
+    public static int caloriesBurntWhileSleeping = 0;
+
+    void Start()
+    {
+        m_ObjectCollider = GetComponent<Collider2D>();
+        Popup.enabled = false;
+        KitchenPopupText.enabled = false;
+        GymPopupText.enabled = false;
+        BedroomPopupText.enabled = false;
+        Debug.Log("Trigger On : " + m_ObjectCollider.isTrigger);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("s"))
+        {
+            caloriesBurntWhileSleeping = caloriesBurntWhileSleeping + 500;
+            //Debug.Log(CalorieScore.calories);
+            SceneManager.LoadScene ("weightlostScene");
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        //GameObject's Collider is now a trigger Collider when the GameObject is clicked. It now acts as a trigger
+        m_ObjectCollider.isTrigger = true;
+        Debug.Log(Popup.enabled);
+        Popup.enabled = true;
+        BedroomPopupText.enabled = true;
+    }
+
+    void OnTriggerExit2D()
+    {
+        Popup.enabled = false;
+        BedroomPopupText.enabled = false;
+        Debug.Log("Fridge");
+    }
+
+}
---
+++ Final/MyCalorie/Assets/Scripts/weightloss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class weightloss : MonoBehaviour {
+
+	public Text weightlossText;
+	private double weightlossValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		weightlossValue = 0.000128 * CalorieScore.calories;
+		weightlossText.text = "Congratulations!! You have lost " +  weightlossValue.ToString() + " Kgs";
+	}
+}
